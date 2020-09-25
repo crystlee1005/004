b061493a37dd16844a03790366b6ac0f47e97fb9
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-09-25 13:34:59 +0900

    E
    실습 6

--- mainn.c
+++ mainn.c
@@ -5,15 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	int year;
-	int result;
-	
-	printf("년도를 입력하세요: ");
-	scanf("%i", &year);
-	
-	
-	result = ((year%4 == 0) && (year%100 != 0)) || ( year%400 == 0);
-	
-	printf(" %i 년도는 윤년일까,,? : %i \n", year, result); 
+   int sec;
+    
+   printf("초를 입력하세요: ");
+   scanf("%i", &sec);
+   
+   printf("%i 초는 %i : %i : %i ", sec, sec/3600, (sec%3600)/60, ((sec%3600)%60) );
+   
 	return 0;
 }
